28b58768a64742eb2ec9bb83b591e50c7f1f5845
Maria Antonya <129337355+mariantonyaf@users.noreply.github.com>
2024-06-12 10:59:19 -0300

    Update script.js

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -3,3 +3,12 @@ const markslist = document.getElementById("marks-list")
 let intervalid = 0
 let timer = 0
 let marks = []
+const formatTime = (time) => {
+    const hours = Math.floor(time/360000)
+    const minutes = Math.floor(time/360000) / 6000
+    const seconds = Math.floor(time/6000) / 100
+    const hundresths = time % 100
+
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${hundresths.toString().padStart(2, '0')}`
+
+}
